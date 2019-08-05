86cdfc44e969902546105c6ac5c51fdcf1f077b3
Mario Alberto Ibarra Manzano <ibarram@users.noreply.github.com>
2019-08-04 21:14:35 -0500

    Update Ejemplo_001.c

--- Ejemplo_001.c
+++ Ejemplo_001.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char *arhv[])
+int main(int argc, char *argv[])
 {
 	int a = 10, b = -5;
-	printf("%-d\t%-d\n", a, b);
+	printf("%-d\t%-d\n", a, b);	
+	printf("%+d\t%+d\n", a, b);
+	printf("% d\t% d\n", a, b);
+	printf("%#d\t%#d\n", a, b);
+	printf("%03d\t%03d\n", a, b);
 	return 0;
 }
